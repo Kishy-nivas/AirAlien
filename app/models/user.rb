fe1105dcb69131bcf2385805824ec2dff924225f
Kishy-nivas <kishynivas10@gmail.com>
2017-12-24 09:19:46 +0530

    changed edit layout

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,19 +6,5 @@ class User < ApplicationRecord
 
   validates :fullname ,presence: true, length: {maximum: 58}
 
-  def self.from_omniauth(auth)
-    user = User.where(email: auth.info.email).first 
-    if user 
-      return user 
-    else 
-      where(provider: auth.provider,uid: auth.uid ).first_or_create do |user| 
-        user.provider = auth.provider 
-        user.uid = auth.uid 
-        user.email = auth.info.email 
-        user.image = auth.info.image 
-        user.password = Devise.friendly_token[0,20]
-
-  end 
-
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'page#home'
 
   devise_for :users
+
+  resources :users, only: [:show]
+  
 end
